bc09ac3b2666a38b86c2d6a1754e92bb20864a5b
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2022-06-15 12:10:41 +0700

    main

--- WEB/module/acropolis_v4/VOID v3/js/univscript.js
+++ WEB/module/acropolis_v4/VOID v3/js/univscript.js
@@ -13,18 +13,18 @@ document.addEventListener("scroll", () => {
 const goToTop = () => {
     document.body.scrollIntoView();
 };
-// document.addEventListener('contextmenu', event => event.preventDefault());
-// document.onkeydown = function (e) {
-//     if (e.keyCode === 123) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.keyCode === 74) {
-//         return false;
-//     }
-//     if (e.ctrlKey && e.keyCode === 85) {
-//         return false;
-//     }
-// }
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function (e) {
+    if (e.keyCode === 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === 73) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode === 74) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode === 85) {
+        return false;
+    }
+}
